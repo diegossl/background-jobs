40be64ee12d4b366665c67bdb1c13584a883bc36
diegossl <diegossl94@gmail.com>
2020-07-02 23:57:50 -0300

    Finished!

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const logger = require('morgan')
 const dotenv = require('dotenv')
+const BullBoard = require('bull-board')
+const QueueService = require('./api/services/QueueService')
 
 dotenv.config()
+
+BullBoard.setQueues(QueueService.queues.map(queue => queue.bull))
 
 const usersRouter = require('./routes/users')
 
@@ -13,5 +17,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', usersRouter)
+app.use('/queues', BullBoard.UI)
 
 module.exports = app
